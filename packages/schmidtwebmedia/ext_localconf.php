2681d3cd77997f1462cd7595570e0480aaeabfee
marco <kontakt@schmidt-webmedia.de>
2022-08-08 08:27:31 +0200

    #BUGFIX Fix some issues from error.log

--- packages/schmidtwebmedia/ext_localconf.php
+++ packages/schmidtwebmedia/ext_localconf.php
@@ -10,7 +10,7 @@ $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['schmidtwebmedia'] = 'EXT:schmidtw
 /***************
  * PageTS
  */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TsConfig/Page/All.tsconfig">');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:schmidtwebmedia/Configuration/TsConfig/Page/All.tsconfig">');
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['beforeRendering'][1571076908]
     = \MarcoSchmidtWebmedia\Schmidtwebmedia\Hooks\FormElementLinkResolverHook::class;
